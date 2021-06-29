db8bf0aaf2a72fc3c0a243ff380b039084145247
CyberCitizen01 <mr.20u10448@btech.nitdgp.ac.in>
2021-06-29 21:53:27 +0530

    Task completed
    now the function make_table in this file, makes the required table with the given conditions. Thank you @python-discord

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -11,9 +11,61 @@ def make_table(rows: List[List[Any]], labels: Optional[List[Any]] = None, center
     :return: A table representing the rows passed in.
     """
     ...
-    def width_of_column(n:int):
+    def width_of_column(column_index:int) -> int:
         from operator import itemgetter
+        column = list( map( itemgetter(column_index), rows ) )
+        column = list( map( str, column ) )
+        column_width = len(max(column, key=len))
         if labels is not None:
-            rows.insert(0,labels)
-        column = list( map( itemgetter(n), rows ) )
-        return len(max(column, key=len))
+            column_width = max(column_width, len(str(labels[column_index])))
+        return column_width
+    
+    column_widths = [width_of_column(column_index) for column_index in range(len(rows[0]))]
+    
+    def spaces_in_row(row:list) -> list:
+        _spaces = []
+        for column_index in range(len(row)):
+            column_width = column_widths[column_index]
+            cell_content_width = len(str(row[column_index]))
+            if centered:
+                left_space = ( ( column_width - cell_content_width ) // 2 ) + 1
+            else:
+                left_space = 1
+            right_space = ( column_width - ( cell_content_width + left_space - 1 ) ) + 1
+            _spaces.append((left_space, right_space))
+        return _spaces
+    
+    def tablify_row(row:list) -> str:
+        table_row = '│'
+        for (left_space,right_space),cell in zip(spaces_in_row(row), row):
+            table_cell = ' '*left_space + str(cell) + ' '*right_space + '│'
+            table_row += table_cell
+        table_row += '\n'
+        return table_row
+
+    def make_border(first_piece:str, middle_piece:str, last_piece:str):
+        _border = ''
+        for dash_line in border_dashes:
+            _border += dash_line + middle_piece
+        _border = first_piece + _border[:-1] + last_piece
+        return _border
+
+    border_dashes = ['─'*( width + 2 ) for width in column_widths]
+    top_border    = make_border('┌','┬','┐\n')
+    bottom_border = make_border('└','┴','┘')
+
+    table = ''+top_border
+
+    if labels is not None:
+        label_border = make_border('├','┼','┤\n')
+        table += tablify_row(labels) + label_border
+
+    for row in rows:
+        table += tablify_row(row)
+    
+    table += bottom_border
+
+    return table
+
+
+
